76f86c59cfdba95c7a3d91fc03fd437a73b33145
Gregory Quinlan <greg@gregquinlan.work>
2017-10-12 14:35:52 -0400

    feat: Add PSSA model

--- aplus/models.py
+++ aplus/models.py
@@ -1,175 +1,77 @@
-#!/usr/bin/env python
+import os
+import sys
 
-import httplib
-import endpoints
-from protorpc import messages
-from google.appengine.ext import ndb
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
-"""models.py
-
-Udacity conference server-side Python App Engine data & ProtoRPC models
-
-$Id: models.py,v 1.1 2014/05/24 22:01:10 wesc Exp $
-
-created/forked from conferences.py by wesc on 2014 may 24
-
-"""
-
-__author__ = 'wesc+api@google.com (Wesley Chun)'
+from datetime import datetime
 
 
-class ConflictException(endpoints.ServiceException):
-    """ConflictException -- exception mapped to HTTP 409 response"""
-    http_status = httplib.CONFLICT
+db = SQLAlchemy()
 
 
-class StringMessage(messages.Message):
-    """StringMessage-- outbound (single) string message"""
-    data = messages.StringField(1, required=True)
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(250), unique=True)
+    picture = db.Column(db.String(250))
+    admin = db.Column(db.Boolean)
+
+    def __init__(self, name, email, picture, admin):
+        self.name = name
+        self.email = email
+        self.picture = picture
+        self.admin = admin
+
+    def __repr__(self):
+        return '<Users %r>' % self.name
 
 
-class BooleanMessage(messages.Message):
-    """BooleanMessage-- outbound Boolean value message"""
-    data = messages.BooleanField(1)
+class PSSA(db.Model):
+    year = db.Column(db.Integer, nullable=False)
+    school = db.Column(db.String(250), nullable=False)
+    subject = db.Column(db.String(250), nullable=False)
+    grade = db.Column(db.String(250), nullable=False)
+    subset = db.Column(db.String(250), nullable=False)
+    total_tested = db.Column(db.Integer)
+    below_basic = db.Column(db.Integer)
+    basic = db.Column(db.Integer)
+    proficient = db.Column(db.Integer)
+    advanced = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
 
+    def __init__(self, year, school, subject, grade, subset, total_tested,
+        below_basic, basic, proficient, advanced):
+        self.year = year
+        self.school = school
+        self.subject = subject
+        self.grade = grade
+        self.subset = subset
+        self.total_tested = total_tested
+        self.below_basic = below_basic
+        self.basic = basic
+        self.proficient = proficient
+        self.advanced = advanced
 
-class Conference(ndb.Model):
-    """Conference -- Conference object"""
-    name = ndb.StringProperty(required=True)
-    description = ndb.StringProperty()
-    organizerUserId = ndb.StringProperty()
-    topics = ndb.StringProperty(repeated=True)
-    city = ndb.StringProperty()
-    startDate = ndb.DateProperty()
-    month = ndb.IntegerProperty()
-    endDate = ndb.DateProperty()
-    maxAttendees = ndb.IntegerProperty()
-    seatsAvailable = ndb.IntegerProperty()
+    def __repr__(self):
+        return '<PSSA Yr: {} Sch: {} Sbj: {} Gr: {} Set: {}>'.format(self.year,
+            self.school, self.subject, self.grade, self.subset)
 
+    @property
+    def serialize(self):
+        # Returns object data in easily serializable format
+        return {
+            'year': self.year,
+            'school': self.school,
+            'subject': self.subject,
+            'grade': self.grade,
+            'subset': self.subset,
+            'total_tested': self.total_tested,
+            'below_basic': self.below_basic,
+            'basic': self.basicm
+            'proficient': self.proficient,
+            'advanced': self.advanced
+        }
 
-class ConferenceForm(messages.Message):
-    """ConferenceForm -- Conference outbound form message"""
-    name = messages.StringField(1)
-    description = messages.StringField(2)
-    organizerUserId = messages.StringField(3)
-    topics = messages.StringField(4, repeated=True)
-    city = messages.StringField(5)
-    startDate = messages.StringField(6)  # DateTimeField()
-    month = messages.IntegerField(7)
-    maxAttendees = messages.IntegerField(8)
-    seatsAvailable = messages.IntegerField(9)
-    endDate = messages.StringField(10)  # DateTimeField()
-    websafeKey = messages.StringField(11)
-    organizerDisplayName = messages.StringField(12)
-
-    
-class ConferenceForms(messages.Message):
-    """ConferenceForms -- multiple Conference outbound form message"""
-    items = messages.MessageField(ConferenceForm, 1, repeated=True)
-
-
-class Speaker(ndb.Model):
-    """Speaker -- Speaker profile object"""
-    displayName = ndb.StringProperty(required=True)
-    mainEmail = ndb.StringProperty()
-    bio = ndb.StringProperty()
-
-
-class SpeakerForm(messages.Message):
-    """SpeakerForm -- Speaker outbound form message"""
-    displayName = messages.StringField(1)
-    mainEmail = messages.StringField(2)
-    bio = messages.StringField(3)
-    creatorUserId = messages.StringField(4)
-    websafeKey = messages.StringField(5)
-
-
-class SpeakerForms(messages.Message):
-    """SpeakerForms -- multiple Speaker outbound form message"""
-    items = messages.MessageField(SpeakerForm, 1, repeated=True)
-
-
-class TeeShirtSize(messages.Enum):
-    """TeeShirtSize -- t-shirt size enumeration value"""
-    NOT_SPECIFIED = 1
-    XS_M = 2
-    XS_W = 3
-    S_M = 4
-    S_W = 5
-    M_M = 6
-    M_W = 7
-    L_M = 8
-    L_W = 9
-    XL_M = 10
-    XL_W = 11
-    XXL_M = 12
-    XXL_W = 13
-    XXXL_M = 14
-    XXXL_W = 15
-
-
-class ConferenceQueryForm(messages.Message):
-    """ConferenceQueryForm -- Conference query inbound form message"""
-    field = messages.StringField(1)
-    operator = messages.StringField(2)
-    value = messages.StringField(3)
-
-
-class ConferenceQueryForms(messages.Message):
-    """ConferenceQueryForms -- multiple ConferenceQueryForm inbound
-    form message"""
-    filters = messages.MessageField(ConferenceQueryForm, 1, repeated=True)
-
-
-class Session(ndb.Model):
-    """Session -- Session object"""
-    name = ndb.StringProperty(required=True)
-    highlights = ndb.StringProperty()
-    speakerKey = ndb.KeyProperty(Speaker)
-    typeOfSession = ndb.StringProperty()
-    duration = ndb.IntegerProperty()
-    date = ndb.DateProperty(required=True)
-    startTime = ndb.TimeProperty(required=True)
-    conferenceKey = ndb.ComputedProperty( lambda self: self.key.parent() )
-
-
-class SessionForm(messages.Message):
-    """SessionForm -- Session outbound form message"""
-    name = messages.StringField(1)
-    highlights = messages.StringField(2)
-    speakerName = messages.StringField(3)
-    typeOfSession = messages.StringField(4)
-    duration = messages.IntegerField(5)
-    date = messages.StringField(6)
-    startTime = messages.StringField(7)
-    websafeKey = messages.StringField(9)
-    conferenceName = messages.StringField(10)
-    speakerKey = messages.StringField(11)
-
-
-
-class SessionForms(messages.Message):
-    """SessionForms -- multiple Session outbound form message"""
-    items = messages.MessageField(SessionForm, 1, repeated=True)
-
-
-class Profile(ndb.Model):
-    """Profile -- User profile object"""
-    displayName = ndb.StringProperty()
-    mainEmail = ndb.StringProperty()
-    teeShirtSize = ndb.StringProperty(default='NOT_SPECIFIED')
-    conferenceKeysToAttend = ndb.KeyProperty(Conference, repeated=True)
-    sessionWishlistKeys = ndb.KeyProperty(Session, repeated=True)
-
-
-class ProfileMiniForm(messages.Message):
-    """ProfileMiniForm -- update Profile form message"""
-    displayName = messages.StringField(1)
-    teeShirtSize = messages.EnumField('TeeShirtSize', 2)
-
-
-class ProfileForm(messages.Message):
-    """ProfileForm -- Profile outbound form message"""
-    displayName = messages.StringField(1)
-    mainEmail = messages.StringField(2)
-    teeShirtSize = messages.EnumField('TeeShirtSize', 3)
