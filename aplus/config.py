3ec98a5acce3beb4a91d7b57b4404efb82a5ad5e
Gregory Quinlan <greg@gregquinlan.work>
2017-10-14 16:45:55 -0400

    feat: Add DB uri

--- aplus/config.py
+++ aplus/config.py
@@ -6,7 +6,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Define the database - we are working with
-# SQLALCHEMY_DATABASE_URI = 'postgres://ryztryqknsyzog:fVxpW9KcpmHAFAqMo1mBcidICf@ec2-107-21-219-109.compute-1.amazonaws.com:5432/d4kcqmr928j0p2'  # noqa
+SQLALCHEMY_DATABASE_URI = 'postgres://iqwfnjnbuvogsx:3e72c5889e1eb835a4bf06c8b4a1bb3e3d972dc0ded92f7a39de6bd6ba3f2a58@ec2-107-22-252-91.compute-1.amazonaws.com:5432/dc7nivdqo1av4p'  # noqa
 
 # Turn off tracking modifications of objects and emiting signals
 SQLALCHEMY_TRACK_MODIFICATIONS = False
