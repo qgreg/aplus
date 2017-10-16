6e2f1e3e89617545d223f785350c53235564b5ba
Gregory Quinlan <greg@gregquinlan.work>
2017-10-16 15:33:09 -0400

    feat: Add chart options to HomeCtrl

--- aplus/static/js/app.js
+++ aplus/static/js/app.js
@@ -11,10 +11,16 @@
  */
 var app = angular.module("aplusApp", ['chart.js', 'ui.bootstrap']);
 
-  app.controller('DoughnutCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
-    $scope.labels = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-    $scope.data = [350, 450, 100];
-
+app.controller('DoughnutCtrl', ['$scope', function ($scope) {
+    $scope.labels = ['Advanced 6.7%', 'Proficient 35.6%', 'Basic 46.7%', 'Below Basic 11.1%'];
+    $scope.data = [.067, .356, .467, .111];
+    $scope.colors = ['#60B064', '#9DDBB5', '#D2A6D2', '#BA66B0'];
+    $scope.options = {
+        legend: {
+            display: true,
+            position: 'bottom',
+        }
+    }
 }]);
 
 
