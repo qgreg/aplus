051319233009452344577582ffac0fb09ae97651
Gregory Quinlan <greg@gregquinlan.work>
2017-10-16 09:47:54 -0400

    feat: Plot single donut chart with angularjs

--- aplus/static/js/app.js
+++ aplus/static/js/app.js
@@ -2,116 +2,19 @@
 
 /**
  * @ngdoc object
- * @name conferenceApp
- * @requires $routeProvider
- * @requires conferenceControllers
+ * @name aplusApp
  * @requires ui.bootstrap
  *
  * @description
  * Root app, which routes and specifies the partial html and controller depending on the url requested.
  *
  */
-var app = angular.module('aplusApp',
-    ['aplusControllers', 'ngRoute', 'ui.bootstrap']).
-    config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider.
-                when('/', {
-                    templateUrl: '/partials/home.html'
-                    controller: 'HomeCtrl'
-                }).
-                otherwise({
-                    redirectTo: '/'
-                });
-        }]);
+var app = angular.module("aplusApp", ['chart.js', 'ui.bootstrap']);
 
-/**
- * @ngdoc filter
- * @name startFrom
- *
- * @description
- * A filter that extracts an array from the specific index.
- *
- */
-app.filter('startFrom', function () {
-    /**
-     * Extracts an array from the specific index.
-     *
-     * @param {Array} data
-     * @param {Integer} start
-     * @returns {Array|*}
-     */
-    var filter = function (data, start) {
-        return data.slice(start);
-    }
-    return filter;
-});
+  app.controller('DoughnutCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.labels = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+    $scope.data = [350, 450, 100];
+
+}]);
 
 
-/**
- * @ngdoc constant
- * @name HTTP_ERRORS
- *
- * @description
- * Holds the constants that represent HTTP error codes.
- *
- */
-app.constant('HTTP_ERRORS', {
-    'UNAUTHORIZED': 401
-});
-
-
-/**
- * @ngdoc service
- * @name oauth2Provider
- *
- * @description
- * Service that holds the OAuth2 information shared across all the pages.
- *
- */
-app.factory('oauth2Provider', function ($modal) {
-    var oauth2Provider = {
-        CLIENT_ID: '637137825577-3cpbv37ib68oihgbgr28tljvt7pbdr6k.apps.googleusercontent.com',
-        SCOPES: 'email profile',
-        signedIn: false
-    }
-
-    /**
-     * Calls the OAuth2 authentication method.
-     */
-    oauth2Provider.signIn = function (callback) {
-        gapi.auth.signIn({
-            'clientid': oauth2Provider.CLIENT_ID,
-            'cookiepolicy': 'single_host_origin',
-            'accesstype': 'online',
-            'approveprompt': 'auto',
-            'scope': oauth2Provider.SCOPES,
-            'callback': callback
-        });
-    };
-
-    /**
-     * Logs out the user.
-     */
-    oauth2Provider.signOut = function () {
-        gapi.auth.signOut();
-        // Explicitly set the invalid access token in order to make the API calls fail.
-        gapi.auth.setToken({access_token: ''})
-        oauth2Provider.signedIn = false;
-    };
-
-    /**
-     * Shows the modal with Google+ sign in button.
-     *
-     * @returns {*|Window}
-     */
-    oauth2Provider.showLoginModal = function() {
-        var modalInstance = $modal.open({
-            templateUrl: '/partials/login.modal.html',
-            controller: 'OAuth2LoginModalCtrl'
-        });
-        return modalInstance;
-    };
-
-    return oauth2Provider;
-});
