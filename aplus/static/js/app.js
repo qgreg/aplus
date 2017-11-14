843da62d66705417816c446b9da7b8bcb972ffa6
Gregory Quinlan <greg@gregquinlan.work>
2017-11-14 05:11:33 -0500

    feat: Add BrowserCtrl and DonutChildCtrl to app,js

--- aplus/static/js/app.js
+++ aplus/static/js/app.js
@@ -16,29 +16,82 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
       $interpolateProvider.endSymbol('a}');
     }]);
 
-app.config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/browser', {
-                    templateUrl: '/static/partials/browser.html',
+app.config(["$routeProvider","$locationProvider", function ($routeProvider,$locationProvider) {
+      $routeProvider
+                .when('/browse', {
+                    templateUrl: '../static/partials/select.html',
                     controller: 'BrowserCtrl'
                 })
                 .when('/compare', {
-                    templateUrl: '/static/partials/donut.html',
+                    templateUrl: '../static/partials/donut.html',
                     controller: 'DoughnutCtrl'
                 })
                 .when('/', {
-                    templateUrl: '/static/partials/home.html'
+                    templateUrl: '../static/partials/home.html'
                 })
                 .otherwise({
                     redirectTo: '/'
                 });
+      $locationProvider.hashPrefix('');
         }]);
 
 app.controller('MainCtrl', ['$route',
   function MainCtrl($route) {
     this.$route = $route;
 }]);
+
+app.controller('BrowserCtrl', ['$scope', '$http', 'dataFactory',
+    function ($scope, $http, dataFactory) {
+
+    $scope.pssa = {
+    "year": "",
+    "subject": "",
+    "school": "",
+    "grade": "",
+    "subset": ""
+    };
+
+    $http.get('/api/pssa/select')
+        .then(function (result) {
+            $scope.select = result.data;
+        });
+
+    $scope.fullid = $scope.pssa.year + $scope.pssa.subject + $scope.pssa.school + $scope.pssa.grade + $scope.pssa.subset;
+    $scope.colors = ['#60B064', '#9DDBB5', '#D2A6D2', '#BA66B0'];
+    $scope.options = {
+        legend: {
+            display: true,
+            position: 'bottom'
+    }};
+
+    $scope.status;
+    $scope.scores;
+    $scope.labels;
+    $scope.data;
+
+    $scope.getScores = function () {
+        dataFactory.getScores($scope.pssa.year, $scope.pssa.school, $scope.pssa.grade, $scope.pssa.subject, $scope.pssa.subset)
+            .then(
+                function (response) {
+                    $scope.status = 'Retrieved score!';
+                    $scope.scores = response.data;
+                    $scope.labels = ['Advanced ' + $scope.scores.advanced + ' ' + ($scope.scores.advanced / $scope.scores.total_tested * 100).toFixed(1) + '%', 'Proficient ' + $scope.scores.proficient + ' ' + ($scope.scores.proficient / $scope.scores.total_tested * 100).toFixed(1) + '%', 'Basic ' + $scope.scores.basic + ' ' + ($scope.scores.basic / $scope.scores.total_tested * 100).toFixed(1) + '%', 'Below Basic ' + $scope.scores.below_basic + ' ' + ($scope.scores.below_basic / $scope.scores.total_tested * 100).toFixed(1) + '%'];
+                    $scope.data = [$scope.scores.advanced, $scope.scores.proficient, $scope.scores.basic, $scope.scores.below_basic];
+                    $scope.donutload = true;
+                    // var myPie = new Chart(document.getElementById($scope.fullid).getContext("2d")).Doughnut($scope.data, $scope.labels, $scope.options);
+                    var canvas = document.getElementById($scope.fullid);
+                    var ctx = canvas.getContext("2d");
+                    ctx.font = "10px Arial";
+                    var topScore = (($scope.scores.advanced + $scope.scores.proficient) * 100).toFixed(1);
+                    ctx.fillText("Hello",0,0);
+                    ctx.fillText(topScore + "%",10,50);
+                }, 
+                function (error) {
+                    $scope.status = 'Error retrieving score! ' + error.message;
+                })
+    };
+}]);
+
 
 app.controller('DoughnutCtrl', ['$scope', 'dataFactory',
     function ($scope, dataFactory) {
@@ -64,6 +117,48 @@ app.controller('DoughnutCtrl', ['$scope', 'dataFactory',
 
     $scope.getScores = function () {
         dataFactory.getScores($scope.year, $scope.school, $scope.grade, $scope.subject, $scope.subset)
+            .then(
+                function (response) {
+                    $scope.status = 'Retrieved score!';
+                    $scope.scores = response.data;
+                    $scope.labels = ['Advanced ' + ($scope.scores.advanced * 100).toFixed(1) + '%', 'Proficient '  + ($scope.scores.proficient * 100).toFixed(1) + '%', 'Basic ' + ($scope.scores.basic * 100).toFixed(1) + '%', 'Below Basic ' + ($scope.scores.below_basic * 100).toFixed(1) + '%'];
+                    $scope.data = [$scope.scores.advanced, $scope.scores.proficient, $scope.scores.basic, $scope.scores.below_basic];
+                    $scope.donutload = true;
+                    // var myPie = new Chart(document.getElementById($scope.fullid).getContext("2d")).Doughnut($scope.data, $scope.labels, $scope.options);
+                    var canvas = document.getElementById($scope.fullid);
+                    var ctx = canvas.getContext("2d");
+                    ctx.font = "10px Arial";
+                    var topScore = (($scope.scores.advanced + $scope.scores.proficient) * 100).toFixed(1);
+                    ctx.fillText("Hello",0,0);
+                    ctx.fillText(topScore + "%",10,50);
+                }, 
+                function (error) {
+                    $scope.status = 'Error retrieving score! ' + error.message;
+                })
+    };
+}]);
+
+app.controller('DonutChildCtrl', ['$scope', 'dataFactory',
+    function ($scope, dataFactory) {
+
+    var vm = this;
+    BrowserCtrl.apply(vm, arguments);
+    $scope.donutload = false;
+    $scope.fullid = pssa.year + pssa.subject + pssa.school + pssa.grade + pssa.subset
+    $scope.colors = ['#60B064', '#9DDBB5', '#D2A6D2', '#BA66B0'];
+    $scope.options = {
+        legend: {
+            display: true,
+            position: 'bottom'
+    }};
+
+    $scope.status;
+    $scope.scores;
+    $scope.labels;
+    $scope.data;
+
+    $scope.getScores = function () {
+        dataFactory.getScores(pssa.year, pssa.school, pssa.grade, pssa.subject, pssa.subset)
             .then(
                 function (response) {
                     $scope.status = 'Retrieved score!';
