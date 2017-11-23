8e4d0b9e8f8e0eb296733e4cafdb1912f0c43bb2
Gregory Quinlan <greg@gregquinlan.work>
2017-11-22 20:36:24 -0500

    feat: Add endpoint for compare two school lists by config feat: Add endpoint for comparing two school all datasets feat: Add endpoint for options for two schools by config

--- aplus/api/views.py
+++ aplus/api/views.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, \
 	jsonify, Blueprint, current_app
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import DatabaseError
+from sqlalchemy import or_
 
 
 from aplus.models import db, PSSA, School
@@ -36,6 +37,36 @@ def apiPSSA(year, school, grade, subject, subset):
 
     return resp
 
+
+@api.route('/api/school/<school>/<year>/<grade>/<subject>/<subset>')
+def apiSchoolPSSA(school, year, grade, subject, subset):
+    if subject == "All" and subset != "All":
+        scores = PSSA.query.filter(PSSA.year==year)\
+            .filter(PSSA.school==school).filter(PSSA.grade==grade)\
+            .filter(PSSA.subset==subset).all()
+    elif subject != "All" and subset == "All":
+        scores = PSSA.query.filter(PSSA.year==year)\
+            .filter(PSSA.school==school).filter(PSSA.grade==grade)\
+            .filter(PSSA.subject==subject).all()
+    elif subject == "All" and subset == "All":
+         scores = PSSA.query.filter(PSSA.year==year)\
+            .filter(PSSA.school==school).filter(PSSA.grade==grade)\
+            .filter(PSSA.subject==subject).all()
+    else:
+        scores = PSSA.query.filter(PSSA.year==year)\
+            .filter(PSSA.school==school).filter(PSSA.grade==grade)\
+            .filter(PSSA.subject==subject).filter(PSSA.subset==subset).all()
+    if scores:
+        print scores
+        output = []
+        for score in scores:
+            output.append(score.serialize)
+        resp = jsonify(data=output)
+    else:
+        return ('', 200)
+
+    return resp
+
 @api.route('/api/pssa/select/')
 def menuSelectJSON():
     """API for browse select drop downs.
@@ -65,11 +96,11 @@ def schoolTypeListJSON(school_type):
 
     Returns: JSON of a list schools of the given type.
     """
-    if school_type == 'elementary':
+    if school_type == 'Elementary':
         configOpts = ['K-5', 'K-8']
-    elif school_type == "middle":
+    elif school_type == "Middle":
         configOpts = ['K-8', '6-8', '6-12']
-    elif school_type == 'high':
+    elif school_type == 'High':
         configOpts = ['6-12', '9-12']
     else:
         configOpts = []
@@ -80,6 +111,7 @@ def schoolTypeListJSON(school_type):
             .filter(School.configuration.in_(configOpts))\
             .order_by(School.school_name).distinct().all()
         if labels:
+            print labels
             unzip = zip(*labels)
             output['School'] = unzip
     resp = jsonify(output)
@@ -87,5 +119,29 @@ def schoolTypeListJSON(school_type):
 
     return resp
 
+
+@api.route('/api/school/option/1/<school_name1>/2/<school_name2>')
+def schoolOptJSON(school_name1, school_name2):
+    """API for list of school by school type.
+    Arguements: 
+        school_name1: string of school name 1
+        school_name2: string of school name 1
+
+    Returns: JSON of a list schools of the given type.
+    """
+    columns = ['year', 'subject', 'grade', 'subset']
+    output = {}
+    for col in columns:
+        labels = db.session.query(getattr(PSSA,col))\
+            .filter(or_(PSSA.school==school_name1, PSSA.school==school_name2))\
+            .order_by(getattr(PSSA,col)).distinct().all()
+        if labels:
+            unzip = zip(*labels)
+            output[col] = unzip
+    print output
+    resp = jsonify(output)
+    resp.status_code = 200
+
+    return resp
 
 
