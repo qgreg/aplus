6d66dfda84d9252b360c8ca438c0da164a6ed656
Gregory Quinlan <greg@gregquinlan.work>
2017-11-22 20:43:38 -0500

    feat: Add CompareCtrl Feat: Remove unused DonutChildCtrl fix: Format for 80 line limit

--- aplus/static/js/app.js
+++ aplus/static/js/app.js
@@ -23,8 +23,8 @@ app.config(["$routeProvider","$locationProvider", function ($routeProvider,$loca
                     controller: 'BrowserCtrl'
                 })
                 .when('/compare', {
-                    templateUrl: '../static/partials/donut.html',
-                    controller: 'DoughnutCtrl'
+                    templateUrl: '../static/partials/compare.html',
+                    controller: 'CompareCtrl'
                 })
                 .when('/', {
                     templateUrl: '../static/partials/home.html'
@@ -56,39 +56,47 @@ app.controller('BrowserCtrl', ['$scope', '$http', 'dataFactory',
             $scope.select = result.data;
         });
 
-    $scope.fullid = $scope.pssa.year + $scope.pssa.subject + $scope.pssa.school + $scope.pssa.grade + $scope.pssa.subset;
-    $scope.colors = ['#60B064', '#9DDBB5', '#D2A6D2', '#BA66B0'];
-    $scope.options = {
-        legend: {
-            display: true,
-            position: 'bottom'
-    }};
-
     $scope.status;
-    $scope.scores;
-    $scope.labels;
-    $scope.data;
 
     $scope.getScores = function () {
-        dataFactory.getScores($scope.pssa.year, $scope.pssa.school, $scope.pssa.grade, $scope.pssa.subject, $scope.pssa.subset)
-            .then(
-                function (response) {
-                    $scope.status = 'Retrieved score!';
-                    $scope.scores = response.data;
-                    if ($scope.scores.hasOwnProperty('total_tested')) {
-                        $scope.labels = ['Advanced ' + $scope.scores.advanced + ' ' + ($scope.scores.advanced / $scope.scores.total_tested * 100).toFixed(1) + '%', 'Proficient ' + $scope.scores.proficient + ' ' + ($scope.scores.proficient / $scope.scores.total_tested * 100).toFixed(1) + '%', 'Basic ' + $scope.scores.basic + ' ' + ($scope.scores.basic / $scope.scores.total_tested * 100).toFixed(1) + '%', 'Below Basic ' + $scope.scores.below_basic + ' ' + ($scope.scores.below_basic / $scope.scores.total_tested * 100).toFixed(1) + '%'];
-                        $scope.data = [$scope.scores.advanced, $scope.scores.proficient, $scope.scores.basic, $scope.scores.below_basic];
-                        // var myPie = new Chart(document.getElementById($scope.fullid).getContext("2d")).Doughnut($scope.data, $scope.labels, $scope.options);
-                        var canvas = document.getElementById($scope.fullid);
-                        var ctx = canvas.getContext("2d");
-                        ctx.font = "10px Arial";
-                        var topScore = (($scope.scores.advanced + $scope.scores.proficient) * 100).toFixed(1);
-                        ctx.fillText("Hello",0,0);
-                        ctx.fillText(topScore + "%",10,50);
-                }}, 
-                function (error) {
-                    $scope.status = 'Error retrieving score! ' + error.message;
-                })
+      dataFactory.getScores($scope.pssa.year, $scope.pssa.school, 
+          $scope.pssa.grade, $scope.pssa.subject, $scope.pssa.subset)
+        .then(
+          function (response) {
+            $scope.status = 'Retrieved score!';
+            $scope.score = response.data;
+            if ($scope.score.hasOwnProperty('total_tested')) {
+              $scope.score.labels = [
+                  'Advanced ' + $scope.score.advanced + ' ' + (
+                      $scope.score.advanced / $scope.score.total_tested 
+                      * 100).toFixed(1) + '%', 
+                  'Proficient ' + $scope.score.proficient + ' ' + (
+                      $scope.score.proficient / $scope.score.total_tested 
+                      * 100).toFixed(1) + '%', 
+                  'Basic ' + $scope.score.basic + ' ' + (
+                      $scope.score.basic / $scope.score.total_tested 
+                      * 100).toFixed(1) + '%', 
+                  'Below Basic ' + $scope.score.below_basic + ' ' + (
+                      $scope.score.below_basic / $scope.score.total_tested 
+                      * 100).toFixed(1) + '%'];
+              $scope.score.data = [
+                  $scope.score.advanced, 
+                  $scope.score.proficient, 
+                  $scope.score.basic, 
+                  $scope.score.below_basic];
+            };
+          $scope.score.fullid = $scope.score.year + $scope.score.subject 
+              + $scope.score.school + $scope.score.grade + $scope.score.subset
+          $scope.colors = ['#60B064', '#9DDBB5', '#D2A6D2', '#BA66B0'];
+          $scope.options = {
+              legend: {
+                  display: true,
+                  position: 'bottom'
+               }};
+          }, 
+          function (error) {
+            $scope.status = 'Error retrieving score! ' + error.message;
+          })
     };
 }]);
 
@@ -138,45 +146,83 @@ app.controller('DoughnutCtrl', ['$scope', 'dataFactory',
     };
 }]);
 
-app.controller('DonutChildCtrl', ['$scope', 'dataFactory',
-    function ($scope, dataFactory) {
+app.controller('CompareCtrl', ['$scope', '$http', 'dataFactory',
+    function ($scope, $http, dataFactory) {
 
-    var vm = this;
-    BrowserCtrl.apply(vm, arguments);
-    $scope.donutload = false;
-    $scope.fullid = pssa.year + pssa.subject + pssa.school + pssa.grade + pssa.subset
-    $scope.colors = ['#60B064', '#9DDBB5', '#D2A6D2', '#BA66B0'];
-    $scope.options = {
-        legend: {
-            display: true,
-            position: 'bottom'
-    }};
+    $scope.schoolTypeOpt = {
+    "Elementary": "Elementary",
+    "Middle": "Middle",
+    "High": "High"
+    };
 
-    $scope.status;
-    $scope.scores;
-    $scope.labels;
-    $scope.data;
+    $scope.schools = {
+    "first": "",
+    "second": "",
+    };
 
-    $scope.getScores = function () {
-        dataFactory.getScores(pssa.year, pssa.school, pssa.grade, pssa.subject, pssa.subset)
+    $scope.pssa = {
+    "year": "",
+    "subject": "",
+    "grade": "",
+    "subset": "",
+    "ready": false
+    };
+
+    $scope.schoolType = "";
+    $scope.schoolList = "";
+    $scope.schoolOpt = "";
+
+    $scope.getSchoolList = function () {
+        $http.get('/api/school/type/' + $scope.schoolType)
+        .then(function (result) {
+            $scope.schoolList = result.data;
+        })};
+
+    $scope.getSchoolOpt = function (first, second) {
+        dataFactory.getSchoolOpt(first, second)
+        .then(function (result) {
+            $scope.schoolOpt = result.data;
+        })};
+
+    $scope.getSchoolScores = function (school, pssa, num) {
+        dataFactory.getSchoolScores(pssa.year, school, pssa.grade, pssa.subject, pssa.subset)
             .then(
                 function (response) {
                     $scope.status = 'Retrieved score!';
-                    $scope.scores = response.data;
-                    $scope.labels = ['Advanced ' + ($scope.scores.advanced * 100).toFixed(1) + '%', 'Proficient '  + ($scope.scores.proficient * 100).toFixed(1) + '%', 'Basic ' + ($scope.scores.basic * 100).toFixed(1) + '%', 'Below Basic ' + ($scope.scores.below_basic * 100).toFixed(1) + '%'];
-                    $scope.data = [$scope.scores.advanced, $scope.scores.proficient, $scope.scores.basic, $scope.scores.below_basic];
-                    $scope.donutload = true;
-                    // var myPie = new Chart(document.getElementById($scope.fullid).getContext("2d")).Doughnut($scope.data, $scope.labels, $scope.options);
-                    var canvas = document.getElementById($scope.fullid);
-                    var ctx = canvas.getContext("2d");
-                    ctx.font = "10px Arial";
-                    var topScore = (($scope.scores.advanced + $scope.scores.proficient) * 100).toFixed(1);
-                    ctx.fillText("Hello",0,0);
-                    ctx.fillText(topScore + "%",10,50);
+                    $scope['scores' + num] = response.data;
                 }, 
                 function (error) {
-                    $scope.status = 'Error retrieving score! ' + error.message;
-                })
-    };
+                    $scope.status = 'Error retrieving score! ' 
+                        + error.message;
+                })};
+
+    $scope.getChartParam = function (score) {
+      if (score.hasOwnProperty('total_tested')) {
+        score.labels = [
+            'Advanced ' + score.advanced + ' ' + (
+                    score.advanced / score.total_tested * 100
+                ).toFixed(1) + '%',
+            'Proficient ' + score.proficient + ' ' + (
+                score.proficient / score.total_tested * 100
+                ).toFixed(1) + '%',
+            'Basic ' + score.basic + ' ' + (
+                score.basic / score.total_tested * 100
+                ).toFixed(1) + '%',
+            'Below Basic ' + score.below_basic + ' ' + (
+                score.below_basic / score.total_tested * 100
+                ).toFixed(1) + '%'];
+        };
+      score.data = [
+          score.advanced, score.proficient, score.basic, score.below_basic];
+      score.fullid = score.year + score.subject + score.school + score.grade 
+          + score.subset
+      $scope.colors = ['#60B064', '#9DDBB5', '#D2A6D2', '#BA66B0'];
+      $scope.options = {
+            legend: {
+                display: true,
+                position: 'bottom'
+            }};
+      };
 }]);
+
 
