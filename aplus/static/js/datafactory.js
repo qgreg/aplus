f6d925a0b07b5aeab18c3591dcfdd8e8b2990ac4
Gregory Quinlan <greg@gregquinlan.work>
2017-11-22 20:42:01 -0500

    feat: Add service for listing schools by type feat: ADd service for listing multiple PSSA results

--- aplus/static/js/datafactory.js
+++ aplus/static/js/datafactory.js
@@ -1,9 +1,28 @@
 angular.module("aplusApp").service('dataFactory', ['$http', function ($http) {
 
-        var urlBase = '/api';
+  var urlBase = '/api';
 
-        this.getScores = function (year, school, grade, subject, subset) {
-            return $http.get(urlBase + '/' + year + '/' + school + '/' + grade + '/' +subject + '/' + subset);
-        };
+  this.getScores = function (
+    year, 
+    school, 
+    grade, 
+    subject, 
+    subset) {
+  return $http.get(urlBase + '/' + year + '/' + school + '/' + grade 
+      + '/' + subject + '/' + subset);
+  };
 
+  this.getSchoolOpt = function (first, second) {
+    return $http.get(urlBase + '/school/option/1/'+ first + '/2/' + second);
+  };
+
+  this.getSchoolScores = function (
+      year, 
+      school, 
+      grade, 
+      subject, 
+      subset) {
+    return $http.get(urlBase + '/school/' + school + '/' + year + '/' + grade 
+        + '/' + subject + '/' + subset);
+  };
     }]);
